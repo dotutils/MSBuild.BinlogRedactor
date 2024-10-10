824e3ecc77a404f0fec9ec4ad1ff9ab516ae31d3
YuliiaKovalova <ykovalova@microsoft.com>
2024-10-10 22:37:17 +0200

    update secrets

--- src/DotUtils.MsBuild.SensitiveDataDetector/PatternsDetector.cs
+++ src/DotUtils.MsBuild.SensitiveDataDetector/PatternsDetector.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-using System.Reflection;
 using System.Text.RegularExpressions;
 using DotUtils.MsBuild.SensitiveDataDetector;
 
